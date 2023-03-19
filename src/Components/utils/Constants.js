6fe27e3c9f63542f68881a05de6648315afa8b99
hamza-sadeq <hamzasadeq41@gmail.com>
2023-03-19 09:54:28 +0200

    Portfolio

---
+++ src/Components/utils/Constants.js
@@ -0,0 +1,263 @@
+import BrushIcon from '@mui/icons-material/Brush';
+import CodeIcon from '@mui/icons-material/Code';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+
+export const links = [
+    {
+        linkName: 'Home',
+        to: '/'
+    },
+    {
+        linkName: 'About',
+        to: '/about'
+    },
+    {
+        linkName: 'Resume',
+        to: '/resume'
+    },
+    {
+        linkName: 'portfolios',
+        to: '/portfolio'
+    },
+    {
+        linkName: 'Contact',
+        to: '/contact'
+    }
+]
+
+export const informations = [
+    {
+        Questions: 'Full Name',
+        Answer: 'Hamza Mohamed'
+    },
+    {
+        Questions: 'Age',
+        Answer: '16 Years Old'
+    },
+    {
+        Questions: 'Nationality',
+        Answer: 'Egyption'
+    },
+    {
+        Questions: 'Languages',
+        Answer: 'Arabic , English'
+    },
+    {
+        Questions: 'Address',
+        Answer: 'Asala City'
+    },
+    {
+        Questions: 'Freelance',
+        Answer: 'Available'
+    }
+]
+
+export const ServiceBox = [
+    {
+        icon: <BrushIcon />,
+        name: 'Web Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.',
+        Aos:  'zoom-in-up'
+    },
+    
+    {
+        icon: <CodeIcon />,
+        name: 'Web Development',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.',
+        Aos:  'zoom-in'
+    },
+    
+    {
+        icon: <PhoneAndroidIcon />,
+        name: 'Responsive Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.',
+        Aos:  'zoom-in-down'
+    }
+    
+]
+
+export const skills = [
+    {
+        skillName: 'HTML5',
+        percent: '100%',
+        Aos: 'fade-up'
+    },
+    {
+        skillName: 'CSS3',
+        percent: '95%',
+        Aos: 'fade-down'
+    },
+    {
+        skillName: 'Javascript',
+        percent: '70%',
+        Aos: 'fade-up'
+    },
+    {
+        skillName: 'React Js',
+        percent: '90%',
+        Aos: 'fade-down'
+    },
+    {
+        skillName: 'Material-Ui',
+        percent: '100%',
+        Aos: 'fade-up'
+    },
+    {
+        skillName: 'TailWind CSS',
+        percent: '70%',
+        Aos: 'fade-down'
+    },
+    {
+        skillName: 'Github',
+        percent: '100%',
+        Aos: 'fade-up'
+    },
+    {
+        skillName: 'Git',
+        percent: '100%',
+        Aos: 'fade-down'
+    }
+]
+
+export const LanguageSkills = [
+    {
+        LanguageName: 'Arabic',
+        Fluently: '100%',
+        Aos: 'fade-right'
+    },
+    {
+        LanguageName: 'English',
+        Fluently: '80%',
+        Aos: "fade-right"
+    }
+]
+
+export const reviews = [
+    {
+        ClientName: 'Youssef Mohamed',
+        Freelancing: 'Fiverr Client',
+        clientReview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+    },
+    {
+        ClientName: 'Marwan Sadek',
+        Freelancing: 'UpWork Client',
+        clientReview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+    },
+    {
+        ClientName: 'Ahmed Ali',
+        Freelancing: 'UpWork CLient',
+        clientReview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+    },
+    {
+        ClientName: 'Mahmoud Mohamed',
+        Freelancing: 'Fiverr Client',
+        clientReview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+    },
+    {
+        ClientName: 'Ammar Sobhy',
+        Freelancing: 'UpWork Client',
+        clientReview: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+    },
+]
+
+export const Work = [
+    {
+        image: '/ProjectImages/8f989c08-175f-40d6-9aab-62ca9fa23fef.png',
+        projectName: 'ROBY',
+        projectVercelLink: 'https://roby-web.vercel.app/',
+        About: '(HTML , CSS , JS , Tailwind CSS)',
+        Aos: 'flip-right'
+    },
+    {
+        image: '/ProjectImages/7546b1e6-5b7e-451f-98e5-0cb9c35b9990.png',
+        projectName: 'Healthy Keepers',
+        projectVercelLink: 'https://healthy-keeperss.vercel.app/',
+        About: '(HTML , CSS , JS , SCSS)',
+        Aos: 'flip-left'
+    },
+    {
+        image: '/ProjectImages/im10.png',
+        projectName: 'Shoe E-commerce',
+        projectVercelLink: 'https://shop-website-nine.vercel.app/',
+        About: '(HTML , CSS)',
+        Aos: 'flip-right'
+    },
+    {
+        image: '/ProjectImages/c9684089-4b84-4bb8-91e3-661666cb367f.png',
+        projectName: 'Delivery',
+        projectVercelLink: 'https://responsive-website-delivery-main.vercel.app/',
+        About: '(HTML , CSS , JS , SCSS)',
+        Aos: 'flip-left'
+    },
+    {
+        image: '/ProjectImages/im2.png',
+        projectName: 'Noon E-Commerce',
+        projectVercelLink: 'https://noon-web.vercel.app/',
+        About: '(HTML , CSS , JS)',
+        Aos: 'flip-right'
+    },
+    {
+        image: '/ProjectImages/339b6097-d1fc-4508-a15b-ee9d40dbad72.png',
+        projectName: 'PLANTEX',
+        projectVercelLink: 'https://plantex-eosin.vercel.app/',
+        About: '(HTML , CSS , JS , SCSS)',
+        Aos: 'flip-left'
+    },
+    {
+        image: '/ProjectImages/im9.png',
+        projectName: 'TimeZone',
+        projectVercelLink: 'https://watchs-six.vercel.app/',
+        About: '(HTML , CSS)',
+        Aos: 'flip-right'
+    },
+    {
+        image: '/ProjectImages/im6.png',
+        projectName: 'Sushi',
+        projectVercelLink: 'https://sushhi.vercel.app/',
+        About: '(HTML , CSS , JS)',
+        Aos: 'flip-left'
+    },
+    {
+        image: '/ProjectImages/im7.png',
+        projectName: 'EleCar',
+        projectVercelLink: 'https://car-lovat.vercel.app/##',
+        About: '(HTML , CSS , JS)',
+        Aos: 'flip-right'
+    },
+]
+
+export const contactInformation = [
+    {
+        icon: <LocalPhoneOutlinedIcon />,
+        title: 'Phone',
+        information: '+2-0100-703-8325'
+    },
+    {
+        icon: <EmailOutlinedIcon />,
+        title: 'Email',
+        information: 'hamzasadeq41@gmail.com'
+    },
+    {
+        icon: <FmdGoodOutlinedIcon />,
+        title: 'Address',
+        information: 'Asala City , New Cairo'
+    },
+]
+
+export const emailForm = [
+    {
+        label: 'Enter you name*',
+        name: 'name'
+    },
+    {
+        label: 'Enter you Email*',
+        name: 'email'
+    },
+    {
+        label: 'Enter you Subject*',
+        name: 'Subject'
+    },
+]
